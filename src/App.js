2c40b040e017b83b7a6b5a50b945feea820809d4
isaacg <iwanyoike65@gmail.com>
2023-09-11 18:28:59 +0300

    Add component to APP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
       <p>display</p>
+      <Navbar />
     </div>
   );
 }
