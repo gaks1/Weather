8743cb6223adddc4f726a2a55741ad13080d2c3a
isaacg <iwanyoike65@gmail.com>
2023-09-12 18:14:02 +0300

    add redux folder

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,11 @@
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>The Dojo Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-    );
-};
+const Navbar = () => (
+  <nav className="navbar">
+    <h1>The Dojo Blog</h1>
+    <div className="links">
+      <a href="/">Home</a>
+      <a href="/create">New Blog</a>
+    </div>
+  </nav>
+);
 
 export default Navbar;
