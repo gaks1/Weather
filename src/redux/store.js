ed6a376520e1e9e491c53b3a6b1371e748e04d92
isaacg <iwanyoike65@gmail.com>
2023-09-28 18:22:05 +0300

    reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,3 +6,14 @@ const initialState = {
     count: 0
 }
 
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return { count: state.count + 1 }
+        case "DECREMENT":
+            return { count: state.count - 1 }
+        default:
+            return state
+    }
+}
